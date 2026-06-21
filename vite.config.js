8153ded24b22a9581efd32b38975b430f286a88e
miguelmorera <info@miguelmorera.com>
2026-06-21 10:34:32 +0200

    feat: Set custom build organization

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   root: 'source',
-  publicDir: 'assets/images',
+  publicDir: false,
   build: {
     outDir: '../build',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (ext === 'webmanifest') {
+            return `[name][extname]`
+          }
+          if (/png|jpe?g|gif|svg|webp|ico/.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`
+          } else if (/woff|woff2|eot|ttf|otf/.test(ext)) {
+            return `assets/fonts/[name]-[hash][extname]`
+          } else if (ext === 'css') {
+            return `assets/css/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        },
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
   },
   server: {
     host: '0.0.0.0',
